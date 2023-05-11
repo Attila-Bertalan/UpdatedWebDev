8ba4e38ef41949f7d668a365ea3da4b13f528732
21008925 <100576677+21008925@users.noreply.github.com>
2023-05-11 21:07:10 +0100

    finished the top half of footer
    going to style bottom half of footer

--- www/footer.php
+++ www/footer.php
@@ -1,32 +1,27 @@
 <div class = "footerContainer">
   <div class="footerTop">
     <div>
-      <div>logo</div>
-      <div>slogan</div>
+      <div>
+        <img src="images/logo.png" class ="logo" width="75" height="65">
+      </div>
+      <div class = "slogan">Made for students,</div>
+      <div class = "slogan">by students!</div>
     </div>
     <div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
+      <div class = "footerTitle">ACE TRAINING</div>
+      <div class = "footerLink"><a href = "index.php">Home</a></div>
+      <div class = "footerLink"><a href = "">News</a></div>
+      <div class = "footerLink"><a href = "">Reviews</a></div>
     </div>
     <div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
+      <div class = "footerTitle">WANT TO GET STARTED?</div>
+      <div class = "footerLink"><a href = "login.php">Login</a></div>
+      <div class = "footerLink"><a href = "register.php">Register</a></div>
     </div>
     <div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-    </div>
-    <div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
-      <div class = "footerLink">link Here</div>
+      <div class = "footerTitle">FIND OUT MORE</div>
+      <div class = "footerLink"><a href = "contact.php">Contact us</a></div>
+      <div class = "footerLink"><a href = "about.php">About Us</a></div>
     </div>
   </div>
   <div class = "line"></div>
