4ab8654c1ef1b2bc0b78a71122d64902d80c4125
21008925 <100576677+21008925@users.noreply.github.com>
2023-05-12 16:28:11 +0100

    ravamped about.php file

--- www/about.php
+++ www/about.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ACE TRAINING </title>
+    <title>ACE TRAINING</title>
     <link rel = "stylesheet" href="style.css">
 
 </head>
@@ -13,24 +13,73 @@
     <div class = "hero">
         <?php include "nav.php" ?>
 
-        <br><br>
-        <div class="why-ace-div">
-            <a class="why-ace">Story of Ace Training</a>
-        </div>
+        <div class = "aboutUsContainer">
+            <div class = "aboutUsContainerIntro">
+                <div class = "aboutIntro"><h1>About Us</h1></div>
+                <div class = "aboutIntro">
+                    <p>
+                        At Ace Training, we are an enthusiastic and dedicated 
+                        community of students who have joined forces to establish 
+                        an innovative online platform that genuinely understands 
+                        and supports the diverse needs of our fellow learners. Our 
+                        primary mission is to empower and uplift students 
+                        originating from all walks of life, providing them 
+                        with the essential resources and tools they require 
+                        to thrive and excel academically.
+                    </p>
+                </div>
+                
+                <div class = "aboutIntro">
+                    <p>
+                        We aim to create a unique educational experience tailored 
+                        to each individual's strengths and weaknesses. Furthermore, 
+                        we strive to foster a collaborative environment where 
+                        students feel encouraged to engage in thought-provoking 
+                        discussions, seek guidance from one another, and 
+                        continuously broaden their academic horizons.
+                    </p>
+                </div>
+            </div>
 
-        <div class="why-ace-text">
-            <br>
-            Ace Training was founded to help students of all ages reach their academic goals. We are an online platform that offers a wide variety of courses, from languages to math and science. 
-            Each course is designed to be engaging and help the student gain the skills they need to succeed. 
-            Our expert instructors are passionate about teaching and offer personalized support and guidance.
-            <br>
-            <br>
-            Ace Training is a website dedicated to providing resources to help students learn. 
-            It was founded in 2010 with the goal of making learning more accessible to individuals and organizations around the world. 
-            Our team consists of experienced educators and professionals from different academic disciplines who have a shared passion for teaching. 
-            We have developed a comprehensive suite of online courses, tutorials, and resources that have helped thousands of learners master their academic subjects.
+            <div class = "aboutTitleTwo"><h1>Our Commitment</h1></div>
+
+            <div class = "infoContainer">
+                <div>
+                    <img src = "images/about-image-one.jpg">
+                </div>
+                <div>
+                    <a class = "aboutText">
+                        At Ace Training, our commitment extends beyond simply 
+                        delivering high-quality educational content, we are also 
+                        passionate about nurturing an inclusive community built 
+                        on mutual respect and shared goals. Our holistic approach 
+                        ensures that every student feels valued, supported, and 
+                        ultimately equipped with the knowledge necessary for 
+                        long-term success both inside and outside the classroom 
+                        setting.
+                    </a>
+                </div>
+            </div>
+
+            <div class = "infoContainer">
+                <div>
+                    <a class = "aboutText">
+                        As a forward-thinking organization, we believe in 
+                        embracing new methodologies that promote active learning, 
+                        critical thinking, and problem-solving abilities. By 
+                        integrating this progressive mindset into our platform, 
+                        we are able to offer a wide array of study materials, 
+                        online courses, tutoring services, and interactive tools 
+                        designed to accommodate learners of all backgrounds and 
+                        skill levels.
+                    </a>
+                </div>
+                <div>
+                    <img src = "images/about-image-two.jpg">
+                </div>
+            </div>
         </div>
-        
+    
         <?php include "footer.php" ?>
     </div>
 </body>
